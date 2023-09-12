312ab7f44c4e857993b3231c1ff0a0d89a3fe5c6
ErvinAtVoco <122527751+ErvinAtVoco@users.noreply.github.com>
2023-09-12 09:45:47 +0300

    Commit 1
    Tuleb välja et ma *ei* oska kasutada git'i korrektselt...

---
+++ index.js
@@ -0,0 +1,52 @@
+//application packages
+const express = require('express');
+const app = express();
+const path = require('path');
+//add template engine
+const hbs = require('express-handlebars');
+//setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname:'hbs',
+    defaultLayout:'main',
+    layoutsDir:__dirname+'/view/layouts/'
+}))
+
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//connect to database
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"joogaa",
+    password:"JoogaaParool123",
+    database:"joga_mysql"
+});
+
+db.connect(function(err){
+    if(err) throw err;
+    console.log('connected to joga_mysql db');
+});
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        console.log(articles)
+        res.render('index', {
+            articles: articles
+        })
+    })
+})
+
+app.listen(3000,() => {
+    console.log('app is started at localhost')
+});
+
+
